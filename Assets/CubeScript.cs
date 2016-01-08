40afbdcf495b3f460661bc1f460b630257efcef2
ludo6577 <ludo6577@gmail.com>
2016-01-08 01:40:49 +0100

    First commit: simple cardboard example scene

---
+++ Assets/CubeScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour {
+
+	public void SetColorRed(){
+		var renderer = GetComponent<Renderer> ();
+		renderer.material.color = Color.red;
+	}
+
+	public void SetColorBlue(){
+		var renderer = GetComponent<Renderer> ();
+		renderer.material.color = Color.blue;
+	}
+}
